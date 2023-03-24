dd1b469a7b0ab415e46f76416ddbe8fd8d575a3a
Yousaf <yousaf@yolbe.com>
2023-03-24 02:24:52 +0000

    deployed to firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("root", root);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
